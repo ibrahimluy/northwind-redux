ae930728ad2f6c767be8f9807ce01ad936d5a8cb
Your Name <ibrahimluy@hotmail.com>
2020-05-01 03:07:25 +0300

    completed

---
+++ src/components/navi/Navi.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+import CartSummary from "../cart/CartSummary";
+import { Link } from "react-router-dom";
+
+const Navi = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>
+          <Link to="/">Northwind Mağazası</Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="/saveproduct">Ürün ekle</Link>
+              </NavLink>
+            </NavItem>
+
+            <CartSummary></CartSummary>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+export default Navi;
---
+++ src/components/root/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
+import { Container } from "reactstrap";
+import { Route, Switch } from "react-router-dom";
+import cartDetail from "../cart/CartDetail";
+import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
+import NotFound from "../common/NotFound";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Navi></Navi>
+        <Switch>
+          <Route path="/" exact component={Dashboard}></Route>
+          <Route path="/product" exact component={Dashboard}></Route>
+          <Route
+            path="/saveproduct/:productId"
+            component={AddOrUpdateProduct}
+          ></Route>
+          <Route path="/saveproduct" component={AddOrUpdateProduct}></Route>
+          <Route path="/cart" exact component={cartDetail}></Route>
+          <Route exact component={NotFound}></Route>
+        </Switch>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
